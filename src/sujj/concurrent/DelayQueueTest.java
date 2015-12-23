d93610ab993206bd4b9a25cf2bcf217da5fcb7c7
thundercrawl <brightpegion@gmail.com>
2015-12-23 16:43:56 +0800

    1

---
+++ src/sujj/concurrent/DelayQueueTest.java
@@ -0,0 +1,15 @@
+package sujj.concurrent;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+
+
+
+public class DelayQueueTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/sujj/concurrent/InterruptTest.java
+++ src/sujj/concurrent/InterruptTest.java
@@ -1,25 +1,36 @@
 package sujj.concurrent;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Timer;
 
 import logger.Log;
+interface InterruptModule
+{
+	public void setExit();
+	public long getId();
+	public void interrupt();
+}
 
 class MonitorThread extends Thread
 {
 	
-	private ArrayList<InterruptTest> _threadStore;
+	private ArrayList<InterruptModule> _threadStore;
 	public MonitorThread()
 	{
-		_threadStore = new ArrayList<InterruptTest>();
+		_threadStore = new ArrayList<InterruptModule>();
 		
 	}
-	public void addThread(InterruptTest obj)
+	public void addThread(InterruptModule obj)
 	{
 		_threadStore.add(obj);
 	}
 	private void stopThreads()
 	{
-		for(InterruptTest intr:_threadStore)
+		for(InterruptModule intr:_threadStore)
 		{
 			Log.info("Stop thread "+ intr.getId());
 			intr.setExit();
@@ -46,11 +57,63 @@ class MonitorThread extends Thread
 	}
 	
 }
-public class InterruptTest extends Thread {
+class IOConnection extends Thread implements InterruptModule {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+			String buffer = "abc";
+			FileOutputStream myErrfile = null;
+			
+			try {
+				myErrfile = new FileOutputStream("c:\\err.log");
+				PrintStream myErr = new PrintStream(myErrfile);
+				Timer timer = new Timer();
+				
+				while(true)
+				{
+					
+					
+					myErr.write(buffer.getBytes());
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			finally
+			{
+				try {
+					logger.Log.info("Error found and exit IOConnection");
+					if(null!=myErrfile)myErrfile.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		
+	}
+
+	@Override
+	public void setExit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void interrupt() {
+		// TODO Auto-generated method stub
+		super.interrupt();
+	}}
+
+
+public class InterruptTest extends Thread implements InterruptModule {
 	public boolean  threadContinue = true;
 	public String block ="block";
 	private boolean _exit = false;
 	private String _threadid;
+	
 	public InterruptTest(String id)
 	{
 		_threadid = id;
@@ -97,17 +160,19 @@ public class InterruptTest extends Thread {
 		InterruptTest t1 = new InterruptTest("1");
 		
 		InterruptTest t2 = new InterruptTest("2");
-		
+		IOConnection ioconn = new IOConnection();
+		ioconn.start();
 		t1.start();
 		t2.start();
 		MonitorThread mt = new MonitorThread();
 		mt.addThread(t1);
 		mt.addThread(t2);
-		
+		mt.addThread(ioconn);
 		mt.start();
 		try {
 			t1.join();
 			t2.join();
+			ioconn.join();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
