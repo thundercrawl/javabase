926908405317ceb596bec5d12728ef9889e8a91b
thundercrawl <brightpegion@gmail.com>
2016-04-23 17:57:20 +0800

    1

---
+++ src/sujj/GC/GCTentureTest.java
@@ -0,0 +1,10 @@
+package sujj.GC;
+
+public class GCTentureTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/sujj/operation/literal/bitwiseTest.java
+++ src/sujj/operation/literal/bitwiseTest.java
@@ -1,10 +1,13 @@
+
 package sujj.operation.literal;
 
 public class bitwiseTest {
 	
 	static public void test()
 	{
-		System.out.println(1L << 64);
+		int st = 2;
+		System.out.println(1L << st);
+		System.out.println(1*st);
 		
 	}
 	public static void main(String[] args) {
