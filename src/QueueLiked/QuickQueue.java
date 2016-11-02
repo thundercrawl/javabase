d0eea5e3d7dba4afbbbda5ab35675b06035d415d
thundercrawl <brightpegion@gmail.com>
2016-11-02 15:21:43 +0800

    1

---
+++ src/QueueLiked/QuickQueue.java
@@ -0,0 +1,50 @@
+package QueueLiked;
+
+import java.util.AbstractQueue;
+import java.util.Iterator;
+
+
+
+class QuickQueueG<E> extends AbstractQueue<E>
+{
+
+	@Override
+	public boolean offer(final E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
+public class QuickQueue {
+
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/sujj/concurrent/SemophoreTest.java
@@ -0,0 +1,10 @@
+package sujj.concurrent;
+import java.util.concurrent.Semaphore;
+public class SemophoreTest {
+
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+		final Semaphore sm = new Semaphore(2);
+	}
+
+}
