30ee3ce8b01a9b5e2aeb8dcc434a51e986c46480
thundercrawl <brightpegion@gmail.com>
2017-02-28 16:00:50 +0800

    src/sujj/proxy/proxyStartup.java

--- src/sujj/proxy/proxyStartup.java
+++ src/sujj/proxy/proxyStartup.java
@@ -1,65 +1,88 @@
 package sujj.proxy;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.List;
 
+import logger.Log;
 
- public class proxyStartup {
-	 private interface modelBehavior
-	 {
-		 public void add();
-		 public void delete();
-	 }
-	private class TestProxy1 implements modelBehavior
-	{
+interface modelBehavior
+{
+	 public void add();
+	 public void delete();
+}
+class TestProxy1 implements modelBehavior
+{
 
-		@Override
-		public void add() {
-			// TODO Auto-generated method stub
-			logger.Log.entering();
-			
-		}
-
-		@Override
-		public void delete() {
-			// TODO Auto-generated method stub
-			logger.Log.entering();
-		}
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		logger.Log.entering();
 		
 	}
-	public class NoOpAddInvocationHandler implements InvocationHandler {
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		logger.Log.entering();
+	}
+	
+}
+
+class QuickHandler implements InvocationHandler {
+	 private Object obj =null;
+	  public Object invoke(Object proxy, Method m, Object[] args) throws Throwable {
+		 Log.info("Proxy invoke called ,method is : "+m.getName());
 		 
-		  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		    if (method.getName().startsWith("add")) {
-		      return false;
-		    }
-		    return method.invoke(args);
-		  }
-		
-		}
+		 return m.invoke(obj, args);
+	  }
+	public QuickHandler(Object obj)
+	{
+		this.obj = obj;
+	}
+	}
+
+ public class proxyStartup {
+	 
 	public proxyStartup() {
 		// TODO Auto-generated constructor stub
 	}
 	public static void proxyT1()
 	{
-		Object proxy =  Proxy.newProxyInstance(
-				  proxyStartup.class.getClassLoader(),
-				  new Class[] { modelBehavior.class },
-				  new NoOpAddInvocationHandler());
-		proxy.toString();
+		Object obj = new TestProxy1();
 		
+		modelBehavior proxy = (modelBehavior) Proxy.newProxyInstance(
+				obj.getClass().getClassLoader(),
+				obj.getClass().getInterfaces(),
+				new QuickHandler(obj));
+		
+		proxy.add();
 		
 	}
-	public static void proxyT2()//add the logs entry for each functions call, that help to listen on the //customized class call
+	//that a combine use case , call the methods any where.
+	public static void proxyT2()
 	{
-		//call some customized functions now
+		proxyT1();
+		
+		new TestProxy1().delete();
 		
 	}
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		// TODO Auto-generated method stub
-		proxyT1();
+		
+		Method[] ms =  proxyStartup.class.getMethods();
+		
+		for(Method m:ms)
+		{
+			if(m.getName().contains("proxyT"))
+			{
+				Log.info("running test case "+m.getName());
+				m.invoke(null,null);
+			}
+		}
 	}
 
 }
