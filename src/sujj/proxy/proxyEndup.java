d4bbf4d81e59703fe8148827210ed71186d4c87f
thundercrawl <brightpegion@gmail.com>
2017-03-01 15:08:52 +0800

    src/sujj/proxy/proxyEndup.java

---
+++ src/sujj/proxy/proxyEndup.java
@@ -0,0 +1,16 @@
+package sujj.proxy;
+
+public class proxyEndup {
+	
+	public static void proxyT1()
+	{}
+	public proxyEndup() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
